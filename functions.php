36e72153fbaa496c9471edc4bcba4554388a21ae
Antonello <lellocaravano@gmail.com>
2016-07-07 12:00:35 +0200

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php
+require_once locate_template('lib/config.php'); // Configuration file 
+require_once locate_template('lib/utils.php'); // Utils functions
+require_once locate_template('lib/enqueue.php'); // WP Enqueue CSS & Scripts
+require_once locate_template('lib/acf.php'); // ACF  
+require_once locate_template('lib/cpt.php'); // Custom Post Types  
