5c5399ac38f9a43e6141362b837ef7af4286f98b
Antonello <lellocaravano@gmail.com>
2016-07-07 11:59:08 +0200

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer>
+	<div class="container">
+		<div class="powered">
+			Powered by <a href="#" target="_blank">Company Name</a>
+		</div>
+	</div>
+</footer>
+<?php wp_footer(); ?>
+</body>
+</html>
